7c00c7759f071b437259388c8189690e753c2464
latorware <davidlatorremotos@gmail.com>
2021-10-11 11:05:55 +0200

    testing classes

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,30 @@
 import IA.Gasolina.CentrosDistribucion;
 import IA.Gasolina.Distribucion;
+import IA.Gasolina.Gasolineras;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        CentrosDistribucion c = new CentrosDistribucion(10, 1, 321);
+
+
+
+
+        // Ejemplo
+
+
+        CentrosDistribucion c = new CentrosDistribucion(10, 5, 321); //10 centros, 5 camiones
 
 
         for(int i = 0; i < c.size(); ++i) {
             System.out.println("Centro " + i + ": " + (c.get(i)).getCoordX() + " " + (c.get(i)).getCoordY());
         }
+
+
+        Gasolineras g = new Gasolineras(50, 321) //50 gasolineras
+
+        for()
+
+
 
 
     }
