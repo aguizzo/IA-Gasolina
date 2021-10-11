987cde5be0b22d4a49fe35fb6c92b7b1446ef33e
latorware <davidlatorremotos@gmail.com>
2021-10-11 21:11:51 +0200

    update goalfunction

--- src/IAGas/GasolinaGoalTest.java
+++ src/IAGas/GasolinaGoalTest.java
@@ -3,5 +3,9 @@ package IAGas;
 import aima.search.framework.GoalTest;
 
 public class GasolinaGoalTest implements GoalTest {
+
+    public boolean isGoalState(Object aState) {
+        GasolinaState state = (GasolinaState) 
+    }
     
 }
