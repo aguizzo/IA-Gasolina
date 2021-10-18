e87632872d08903b049c6058c4e48a42bace0a21
miquelflorensa <91897595+miquelflorensa@users.noreply.github.com>
2021-10-18 21:55:21 +0200

    Update GasolinaSuccessorFunction1.java

--- src/IAGas/GasolinaSuccessorFunction1.java
+++ src/IAGas/GasolinaSuccessorFunction1.java
@@ -4,17 +4,21 @@ import java.util.List;
 import aima.search.framework.SuccessorFunction;
 import aima.search.framework.Successor;
 
-//HILL CLIMBING
 public class GasolinaSuccessorFunction1 implements SuccessorFunction {
     public List getSuccessors(Object state) {
-        ArrayList retval = new ArrayList();
+        ArrayList<Successor> retval = new ArrayList<>();
         GasolinaState board = (GasolinaState) state;
 
-        GasolinaState new_state = new GasolinaState(board);
-        // Per a cada cmió. fer un estat per a totes les gasolineres.
+
+        // Per a cada camió. fer un estat per a totes les gasolineres.
         for (int i = 0; i < GasolinaState.centros.size(); ++i) {
             for (int j = 0; j < GasolinaState.gas.size(); ++j) {
-                if (new_state.addGasolinera(i,j)) retval.add(new Successor(new String(), new_state));
+                GasolinaState new_state = new GasolinaState(board);
+                if (new_state.addGasolinera(i,j)) {
+                    StringBuffer S = new StringBuffer();
+                    S.append("s'ha afegit la gasolinera " + j + " al camió " + i + ". Beneficis: " + new_state.beneficis);
+                    retval.add(new Successor(S.toString(), new_state));
+                }
             }
         }
         return retval;
