f85415f2be6e37059e0cebf0e93fbfc3df861a90
miquelflorensa <91897595+miquelflorensa@users.noreply.github.com>
2021-10-18 16:01:15 +0200

    Update GasolinaSuccessorFunction1.java

--- src/IAGas/GasolinaSuccessorFunction1.java
+++ src/IAGas/GasolinaSuccessorFunction1.java
@@ -1,9 +1,26 @@
 package IAGas;
-
+import java.util.ArrayList;
+import java.util.List;
 import aima.search.framework.SuccessorFunction;
 import aima.search.framework.Successor;
 
 //HILL CLIMBING
 public class GasolinaSuccessorFunction1 implements SuccessorFunction {
-    
+    public List getSuccessors(Object state) {
+        ArrayList retval = new ArrayList();
+        GasolinaState board = (GasolinaState) state;
+
+        GasolinaState new_state = new GasolinaState(board);
+        for (int i = 0; i < GasolinaState.centros.size(); ++i) {
+            for (int j = 0; j < GasolinaState.gas.size(); ++j) {
+                new_state.addGasolinera(i,j);
+                for ()
+                while () //camio pugui repartir reparteixi
+            }
+
+            retval.add(new Successor(new String(), new_state));
+
+        }
+        return retval;
+    }
 }
