bdae66ce16d96c8aee1edaaf59570a71a17d3410
miquelflorensa <91897595+miquelflorensa@users.noreply.github.com>
2021-10-18 22:05:48 +0200

    Update GasolinaHeuristic.java

--- src/IAGas/GasolinaHeuristic.java
+++ src/IAGas/GasolinaHeuristic.java
@@ -2,6 +2,9 @@ package IAGas;
 
 import aima.search.framework.HeuristicFunction;
 
-public class GasolinaHeuristic implements HeuristicFunction {
-    
+public class GasolinaHeuristic1 implements HeuristicFunction {
+    public double getHeuristicValue(Object n){
+
+        return ((GasolinaState) n).heuristic();
+    }
 }
