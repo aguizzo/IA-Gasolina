a47cebebcd4d92ae86e71571415e0f2679279a46
latorware <davidlatorremotos@gmail.com>
2021-10-22 18:29:06 +0200

    successor aleanning ended. Needs to be tested

--- src/IAGas/GasolinaSuccessorFunction2.java
+++ src/IAGas/GasolinaSuccessorFunction2.java
@@ -21,14 +21,42 @@ public class GasolinaSuccessorFunction2 implements SuccessorFunction {
         if (opcio == 1) {
             int tipus_metode = GasolinaState.randInt(0, 1); // 0 = add    1 = swap
 
-            int camio_1 = GasolinaState.randInt(0, (GasolinaState.centros.size()-1)); 
+            int camio_1; 
+
+            if (tipus_metode == 0) {
+                int gasolinera; 
+                do {
+                    camio_1 =  GasolinaState.randInt(0, (GasolinaState.centros.size()-1)); 
+                    gasolinera = GasolinaState.randInt(0, (GasolinaState.gas.size()-1)); 
+                } while (!new_state.addGasolinera(camio_1,gasolinera)); 
+
+                StringBuffer S = new StringBuffer();
+                S.append("s'ha afegit la gasolinera " + gasolinera + " al camió " + camio_1 + ". Beneficis: " + new_state.beneficis);
+                retval.add(new Successor(S.toString(), new_state));
+
+
+            }
+
+            else if (tipus_metode == 1) {
+                int camio_2; 
+                do {
+                    camio_1 =  GasolinaState.randInt(0, (GasolinaState.centros.size()-1)); 
+                    do {
+                        camio_2 = GasolinaState.randInt(0, (GasolinaState.centros.size()-1)); 
+                    } while (camio_1 == camio_2); 
+                } while (!new_state.swap(camio_1,camio_2)); 
+
+                    StringBuffer S = new StringBuffer();
+                    S.append("s'ha fet swap entre el ultim viatge del camió " + camio_1 + " i el ultim del camió " + camio_2 + ". Beneficis: " + new_state.beneficis);
+                    retval.add(new Successor(S.toString(), new_state));
+            }
 
 
 
         }
 
 
-        //Igual que la opcio 1, pero si no es pot fer la operacio pel camio i gas triats, no es fa res
+        //Igual que la opcio 1, pero si no es pot fer la operacio pel camio i gas triats (o camio i camio), no es fa res
         else if (opcio == 2) {
             int tipus_metode = GasolinaState.randInt(0, 1); // 0 = add    1 = swap
 
@@ -44,7 +72,10 @@ public class GasolinaSuccessorFunction2 implements SuccessorFunction {
             }
 
             else if (tipus_metode == 1) {
-                int camio_2 = GasolinaState.randInt(0, (GasolinaState.centros.size()-1)); 
+                int camio_2; 
+                do {
+                    camio_2 = GasolinaState.randInt(0, (GasolinaState.centros.size()-1)); 
+                } while (camio_1 == camio_2); 
                 if (new_state.swap(camio_1,camio_2)) {
                     StringBuffer S = new StringBuffer();
                     S.append("s'ha fet swap entre el ultim viatge del camió " + camio_1 + " i el ultim del camió " + camio_2 + ". Beneficis: " + new_state.beneficis);
@@ -54,16 +85,112 @@ public class GasolinaSuccessorFunction2 implements SuccessorFunction {
         }
 
        
-        //Primer es tria un camio i gas random
-        //Lavors es tria de forma random si es fa operacio add o swap. Si no es pot fer la triada es fa laltre
-        //En cas que no es pugui fer cap de les dos, es tria un altre camio i gas randoms
+        //Igual que la opció 1, pero si no es pot fer la operacio pel camio i gas triasts (o camio i camio), s'intenta fer l'altre operacio. Si cap de les dos funciona, no es fa res
         else if (opcio == 3) {
+            int tipus_metode = GasolinaState.randInt(0, 1); // 0 = add    1 = swap
+
+            int camio_1 = GasolinaState.randInt(0, (GasolinaState.centros.size()-1)); 
+
+            if (tipus_metode == 0) {
+                int gasolinera = GasolinaState.randInt(0, (GasolinaState.gas.size()-1)); 
+                if (new_state.addGasolinera(camio_1,gasolinera)) {
+                    StringBuffer S = new StringBuffer();
+                    S.append("s'ha afegit la gasolinera " + gasolinera + " al camió " + camio_1 + ". Beneficis: " + new_state.beneficis);
+                    retval.add(new Successor(S.toString(), new_state));
+                }
+                else { //intentar laltre operacio
+                    int camio_2; 
+                    do {
+                        camio_2 = GasolinaState.randInt(0, (GasolinaState.centros.size()-1)); 
+                    } while (camio_1 == camio_2); 
+                    if (new_state.swap(camio_1,camio_2)) {
+                        StringBuffer S = new StringBuffer();
+                        S.append("s'ha fet swap entre el ultim viatge del camió " + camio_1 + " i el ultim del camió " + camio_2 + ". Beneficis: " + new_state.beneficis);
+                        retval.add(new Successor(S.toString(), new_state));
+                    }
+                }
+
+            }
+
+            else if (tipus_metode == 1) {
+                int camio_2; 
+                do {
+                    camio_2 = GasolinaState.randInt(0, (GasolinaState.centros.size()-1)); 
+                } while (camio_1 == camio_2); 
+                if (new_state.swap(camio_1,camio_2)) {
+                    StringBuffer S = new StringBuffer();
+                    S.append("s'ha fet swap entre el ultim viatge del camió " + camio_1 + " i el ultim del camió " + camio_2 + ". Beneficis: " + new_state.beneficis);
+                    retval.add(new Successor(S.toString(), new_state));
+                }
+                else { //intentar laltre operacio
+                    int gasolinera = GasolinaState.randInt(0, (GasolinaState.gas.size()-1)); 
+                    if (new_state.addGasolinera(camio_1,gasolinera)) {
+                        StringBuffer S = new StringBuffer();
+                        S.append("s'ha afegit la gasolinera " + gasolinera + " al camió " + camio_1 + ". Beneficis: " + new_state.beneficis);
+                        retval.add(new Successor(S.toString(), new_state));
+                    }
+                }
+            }
+
 
         }
 
 
-        //Igual que la opcio 3, pero si no es pot fer cap de les dos operacions, llavors no es fa res mes
+        //Igual que la opció 1, pero si la operacio no es pot fer, abans de triar un altre camio i gas (o camio i camio), s'intenta fer laltre operacio. Aixo tambe es fa pels successius camions i gasolineres (o camions i camions)
         else if (opcio == 4) {
+            int tipus_metode = GasolinaState.randInt(0, 1); // 0 = add    1 = swap
+
+            int camio_1; 
+            int camio_2; 
+            int gasolinera; 
+
+            if (tipus_metode == 0) {
+                do {
+                    camio_1 =  GasolinaState.randInt(0, (GasolinaState.centros.size()-1)); 
+                    gasolinera = GasolinaState.randInt(0, (GasolinaState.gas.size()-1)); 
+                    do {
+                        camio_2 = GasolinaState.randInt(0, (GasolinaState.centros.size()-1)); 
+                    } while (camio_1 == camio_2); 
+                    if (new_state.addGasolinera(camio_1,gasolinera)) {
+                        StringBuffer S = new StringBuffer();
+                        S.append("s'ha afegit la gasolinera " + gasolinera + " al camió " + camio_1 + ". Beneficis: " + new_state.beneficis);
+                        retval.add(new Successor(S.toString(), new_state));
+                        break; 
+                    }
+                    else if (new_state.swap(camio_1,camio_2)) {
+                        StringBuffer S = new StringBuffer();
+                        S.append("s'ha fet swap entre el ultim viatge del camió " + camio_1 + " i el ultim del camió " + camio_2 + ". Beneficis: " + new_state.beneficis);
+                        retval.add(new Successor(S.toString(), new_state));
+                        break; 
+                    }
+                } while (true); 
+
+
+            }
+
+            else if (tipus_metode == 1) {
+                do {
+                    camio_1 =  GasolinaState.randInt(0, (GasolinaState.centros.size()-1)); 
+                    gasolinera = GasolinaState.randInt(0, (GasolinaState.gas.size()-1)); 
+                    do {
+                        camio_2 = GasolinaState.randInt(0, (GasolinaState.centros.size()-1)); 
+                    } while (camio_1 == camio_2); 
+                    if (new_state.swap(camio_1,camio_2)) {
+                        StringBuffer S = new StringBuffer();
+                        S.append("s'ha fet swap entre el ultim viatge del camió " + camio_1 + " i el ultim del camió " + camio_2 + ". Beneficis: " + new_state.beneficis);
+                        retval.add(new Successor(S.toString(), new_state));
+                        break; 
+                    }
+                    else if (new_state.addGasolinera(camio_1,gasolinera)) {
+                        StringBuffer S = new StringBuffer();
+                        S.append("s'ha afegit la gasolinera " + gasolinera + " al camió " + camio_1 + ". Beneficis: " + new_state.beneficis);
+                        retval.add(new Successor(S.toString(), new_state));
+                        break; 
+                    }
+                } while (true); 
+            }
+
+
 
         }
 
